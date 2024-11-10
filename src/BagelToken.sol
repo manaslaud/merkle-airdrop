828ed7f6f0fa83c596277bfa85c7e0c622fb108e
manaslaud <manaslaud@hotmail.com>
2024-11-10 21:38:48 +0530

    feat: basic airdrop

---
+++ src/BagelToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+contract BagelToken is ERC20, Ownable {
+    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
+    function mint(address to, uint256 amt) external onlyOwner {
+        _mint(to, amt);
+    }
+}
